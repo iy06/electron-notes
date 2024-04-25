71b496cf31f1b026fd2cca938fe9b3e083fdae3a
iy06 <yuuki9916@gmail.com>
2024-04-25 19:13:10 +0900

    utils/index.tsに編集時間をformatする関数を追加 cn関数にstringの型を追加

--- app/src/renderer/src/utils/index.ts
+++ app/src/renderer/src/utils/index.ts
@@ -1,6 +1,14 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export const cn = (...args: ClassValue[]) => {
+const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'Asia/Tokyo'
+})
+
+export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)
+
+export const cn = (...args: ClassValue[]): string => {
   return twMerge(clsx(...args))
 }
