44825264a8b1a567a5a0b80c0747287596bc1590
iy06 <yuuki9916@gmail.com>
2024-04-27 16:50:39 +0900

    useNoteListの不要な改行を削除

--- app/src/renderer/src/hooks/useNotesList.tsx
+++ app/src/renderer/src/hooks/useNotesList.tsx
@@ -4,7 +4,6 @@ import { useAtom, useAtomValue } from 'jotai'
 export const useNotesList = ({ onSelect }: { onSelect?: () => void }) => {
   const notes = useAtomValue(notesAtom)
   const [selectedNoteIndex, setSelectedNoteIndex] = useAtom(selectedNoteIndexAtom)
-
   const handleNoteSelect = (index: number) => async () => {
     setSelectedNoteIndex(index)
     if (onSelect) {
