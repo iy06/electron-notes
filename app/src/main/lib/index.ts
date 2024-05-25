a570845130eafb518ca66b82c1bfb78f6b016bfb
iy06 <yuuki9916@gmail.com>
2024-05-25 15:04:17 +0900

    ipcMain.handleでmain/indextsにgetNotesを登録する。 contextBrideでrendererに渡すgetNotesを登録する。 contextの型にgetNotesを追加する。 types.tsにGetNotesの型を定義する。

---
+++ app/src/main/lib/index.ts
@@ -0,0 +1,27 @@
+import { appDirectoryName, fileEncoding } from '@shared/constants'
+import { ensureDir, readdir, stat } from 'fs-extra'
+import { homedir } from 'os'
+import { GetNotes } from '@shared/types'
+
+export const getRootDir = () => {
+  return `${homedir()}/${appDirectoryName}`
+}
+
+export const getNotes: GetNotes = async () => {
+  const rootDir = getRootDir()
+  await ensureDir(rootDir)
+  const notesFileNames = await readdir(rootDir, {
+    encode: fileEncoding,
+    withFileTypes: false
+  })
+  const notes = notesFileNames.filter((fileName) => fileName.endsWith('.md'))
+  return Promise.all(notes.map(getNoteInfoFromFileName))
+}
+
+export const getNoteInfoFromFileName = async (fileName: string): Promise<NoteInfo> => {
+  const fileStats = await stat(`${getRootDir()}/${fileName}`)
+  return {
+    title: fileName.replace(/\.md$/, ''),
+    lastEditTime: fileStats.mtimeMs
+  }
+}
